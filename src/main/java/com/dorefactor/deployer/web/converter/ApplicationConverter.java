65b14726585b187bf1808744ecc5f2ac0ce56ff8
Francisco Castellanos <fvcastellanos@gmail.com>
2019-08-18 12:49:17 -0600

    add applicationView converter

--- src/main/java/com/dorefactor/deployer/web/converter/ApplicationConverter.java
+++ src/main/java/com/dorefactor/deployer/web/converter/ApplicationConverter.java
@@ -1,42 +1,49 @@
 package com.dorefactor.deployer.web.converter;
 
-import static java.util.Objects.isNull;
-
-import java.util.Map;
-
 import com.dorefactor.deployer.domain.model.Application;
 import com.dorefactor.deployer.domain.model.ApplicationSetup;
-import com.dorefactor.deployer.domain.model.ApplicationType;
 import com.dorefactor.deployer.domain.model.docker.DockerApplicationSetup;
 import com.dorefactor.deployer.domain.model.docker.Image;
 import com.dorefactor.deployer.domain.model.docker.Registry;
+import com.dorefactor.deployer.domain.web.view.application.ApplicationSetupView;
 import com.dorefactor.deployer.domain.web.view.application.ApplicationView;
 import com.dorefactor.deployer.domain.web.view.application.docker.DockerApplicationSetupView;
 import com.dorefactor.deployer.domain.web.view.application.docker.ImageView;
 import com.dorefactor.deployer.domain.web.view.application.docker.RegistryView;
-import com.google.common.collect.ImmutableMap;
+
+import static com.dorefactor.deployer.domain.model.ApplicationType.DOCKER;
+import static java.util.Objects.isNull;
 
 public class ApplicationConverter {
 
-    private static final Map<ApplicationType, ApplicationSetup> appTypeMap = 
-        ImmutableMap.of(
-            ApplicationType.DOCKER, 
-        );
+    public static ApplicationView buildApplicationView(Application application) {
 
-    private static ApplicationView buildApplicationView(Application application) {
-        
         var view = new ApplicationView();
         view.setId(application.getId().toString());
         view.setName(application.getName());
 
-        var applicationSetup = application.getApplicationSetup();
-
+        var applicationSetupView = buildApplicationSetupView(application.getApplicationSetup());
+        view.setApplicationSetup(applicationSetupView);
 
         return view;
     }
 
-    private static ApplicationSetup getApplicationSetup(ApplicationSetup applicationSetup) {
+    // ------------------------------------------------------------------------------------------------------------
 
+    private static ApplicationSetupView buildApplicationSetupView(ApplicationSetup applicationSetup) {
+
+        if (isNull(applicationSetup)) {
+
+            return null;
+        }
+
+        if (applicationSetup.getApplicationType().equals(DOCKER)) {
+
+            var dockerAppSetup = (DockerApplicationSetup) applicationSetup;
+            return buildDockerApplicationSetupView(dockerAppSetup);
+        }
+
+        throw new RuntimeException("Application Type Not Implemented");
     }
 
     private static DockerApplicationSetupView buildDockerApplicationSetupView(DockerApplicationSetup dockerApplicationSetup) {
